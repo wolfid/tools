b843f9356b0bcec445307f4d62c9b6f396b902e6
wdhotgen <wolfgangd@hotgen.com>
2017-05-12 12:36:16 +0100

    More formatting

--- SONIX/fwsetdate.bat
+++ SONIX/fwsetdate.bat
@@ -9,14 +9,15 @@ for /f "tokens=1,2,3 delims=:" %%i in ("%time%") do set hour=%%i && set min=%%j 
 for /f "tokens=3" %%i in ('systeminfo ^| find "Time Zone"') do set timezone=%%i
 set yyyy=%yyyy: =%
 if "%yyyy:~0,1%"=="0" set yyyy=%yyyy:~1%
-set mm=%mm:~0,2%
+set mm=%mm: =%
 if "%mm:~0,1%"=="0" set mm=%mm:~1%
-set dd=%dd:~0,2%
+set dd=%dd: =%
 if "%dd:~0,1%"=="0" set dd=%dd:~1%
-set hour=%hour:~0,2%
+set hour=%hour: =%
 if "%hour:~0,1%"=="0" set hour=%hour:~1%
-set min=%min:~0,2%
+set min=%min: =%
 if "%min:~0,1%"=="0" set min=%min:~1%
+set sec=%sec: =%
 set sec=%sec:~0,2%
 if "%sec:~0,1%"=="0" set sec=%sec:~1%
 echo ###########################################################
