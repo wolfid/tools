9bcfd972ebf905dc31e78706134b09ed2a682c34
wdhotgen <wolfgangd@hotgen.com>
2017-06-02 17:38:54 +0100

    Interpret JSON response

--- SONIX/sendjsoncmd.bat
+++ SONIX/sendjsoncmd.bat
@@ -1,6 +1,7 @@
 @echo off
 set ENVCHK="%~dp0..\%DEVCOM%\envchk%~x0"
 set SETENV="%~dp0..\%DEVPRJ%\setenv%~x0"
+set RETOUT="%~dp0..\%DEVPRJ%\%~n0.out"
 echo ###########################################################
 echo ###                             ~\%DEVPRJ%\%~nx0 ###
 echo ###                                    %~t0 ###
@@ -25,10 +26,25 @@ echo ###########################################################
 echo ### Sending "{%CMDSTR%}" to %TGTADR%:%TGTPRT%
 echo ###########################################################
 if "%INTMOD%"=="y" pause
-echo "{%CMDSTR%}" | %NCTEXE% -n -w%TGTTIM% %TGTADR% %TGTPRT%
+echo "{%CMDSTR%}" | %NCTEXE% -n -w%TGTTIM% %TGTADR% %TGTPRT% > %RETOUT%
+for /f "tokens=5" %%i in ('type %RETOUT%') do set RETVAL=%%i
+if "%RETVAL%"=="" goto :RETERR
+set /a RETVAL-=65536
+set /a RETTYP=%RETVAL% / 256
+set /a RETTYP*=256
+set /a RETVAL-=%RETTYP%
+set /a RETTYP/=256
+echo ###########################################################
+echo ### Return Type: %RETTYP%; Return Status: %RETVAL%
+echo ###########################################################
+goto :END
+:RETERR
+echo ###########################################################
+echo ### No response - make sure you are connected...
+echo ###########################################################
 goto :END
 :JSNERR
 echo ###########################################################
-echo ### Need at least one argument
+echo ### Need at least one argument...
 echo ###########################################################
 :END
