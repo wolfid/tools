4de384d59815bc726afb9efed600e2637e6c06f5
wdhotgen <wolfgangd@hotgen.com>
2017-08-15 17:47:19 +0100

    Fix title mode.

--- fwexec.bat
+++ fwexec.bat
@@ -1,5 +1,6 @@
 @echo off
 set ENVCHK="%~dp0Common\envchk%~x0"
+set MODCHK="%~dp0Common\modchk%~x0"
 set SETMOD="%~dp0setmod%~x0"
 set DSPTTL="%~dp0title%~x0"
 set SETPRJ="%~dp0setprj%~x0"
@@ -18,9 +19,13 @@ echo ###########################################################
 echo ### Common Directory: %DEVCOM%
 echo ###########################################################
 if "%1"=="" goto :COMEXE
+if not "%2"=="" goto :PRJEXE
+call %MODCHK% %1 "%QMDLST:"=%_"%NMDLST:"=%
+if "%MODRET%"=="" goto :PRJEXE
+call %ENVCHK% JSTDIT %SETMOD% %1
+goto :COMEXE
+:PRJEXE
 call %ENVCHK% JSTDIT %SETMOD% %2
-if "%DSPMOD%"=="" call %ENVCHK% JSTDIT %SETMOD% %1
-if "%DSPMOD%"=="" goto :COMEXE
 set COMEXE="%~dp0%DEVCOM%\%~1%~x0"
 set PRJEXE="%~dp0%DEVPRJ%\%~1%~x0"
 shift
