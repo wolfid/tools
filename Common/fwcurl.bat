9b348be0071d4290f85b8c73682fe7f9cdd99bc1
wdhotgen <wolfgangd@hotgen.com>
2017-08-22 17:33:00 +0100

    Support motor board fw update.

--- Common/fwcurl.bat
+++ Common/fwcurl.bat
@@ -1,2 +1,2 @@
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
 "C:\Program Files (x86)\Curl\curl.exe" -F "file=@Z:\sonix_firmware\Mebo2.151a\snx_sdk\image\dashcam\FIRMWARE_660R.bin" http://192.168.99.1/ajax/command.json^?command1^=file_upload()^&uploadtype=sonix
--- Mebo/fwupgrade.bat
+++ Mebo/fwupgrade.bat
@@ -6,10 +6,28 @@ echo ###                                ~\%DEVPRJ%\%~nx0 ###
 echo ###                                    %~t0 ###
 echo ###########################################################
 call %ENVCHK% TGTADR %SETENV%
-set FILSTR="file=@%PRJDRV%:\%SUBDIR:"=%\%DEVBRA%\%SDKDIR%\%IMGDIR:"=%\%IMGTYP:"=%\%IMGBIN%.%IMGEXT%"
-set CMDSTR="http://%TGTADR%/ajax/command.json^?command1^=file_upload()^&uploadtype=sonix"
+set CMDSTR="http://%TGTADR%/ajax/command.json^?command1^=file_upload()^&uploadtype="
+if not "%UPGMOD%"=="1" goto :MBUPGD
+echo ###########################################################
+echo ### WiFi Firmware Upgrade
+echo ###########################################################
+set CMDSTR="%CMDSTR:"=%sonix"
+if "%UPGLTT%"=="USE_LATEST" (set FILSTR="file=@%PRJDRV%:\%SUBDIR:"=%\%DEVBRA%\%SDKDIR%\%IMGDIR:"=%\%IMGTYP:"=%\%IMGBIN%.%IMGEXT%"
+) else set FILSTR="file=@%ISSDRV%:\%ISSDIR:"=%\%PRDDIR:"=%\%FMWPRF%%SNXPRF%%PRDCOD%%SNXSUF%%SNXREV%_%SNXDAT%\%IMGBIN%.%IMGEXT%"
+goto :RUNDLL
+:MBUPGD
+echo ###########################################################
+echo ### MotorBoard Firmware Upgrade
+echo ###########################################################
+set CMDSTR="%CMDSTR:"=%MotorBoard"
+set FILSTR="file=@%ISSDRV%:\%ISSDIR:"=%\%PRDDIR:"=%\%FMWPRF%%MOBPRF%%PRDCOD%%MOBSUF%%MOBREV%_%MOBDAT%\%MOBBIN%.%MOBEXT%"
+:RUNDLL
+echo ###########################################################
+echo ### Clearing Internet Form Data
+echo ###########################################################
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
 echo ###########################################################
 echo ### Sending %FILSTR% to %CMDSTR:"=%
 echo ###########################################################
-RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
 %CRLEXE% -F %FILSTR% %CMDSTR:"=%
+:END
