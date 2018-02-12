02bbefe700bbdc0b5396dc1b73ab1c95c6367eee
wdhotgen <wolfgangd@hotgen.com>
2018-02-12 17:47:58 +0000

    Optimised.

--- Common/getrev.bat
+++ Common/getrev.bat
@@ -2,10 +2,10 @@
 set MODCHK="%~dp0..\Common\modchk%~x0"
 call %MODCHK% %1 %QMDLST%_%NMDLST%
 if not "%MODRET%"=="" shift
-echo ###########################################################
-echo ### Get Source Control Revison from: %1
-echo ###########################################################
 if "%~1"=="" goto :END
+echo ###########################################################
+echo ### Get Source Control Revision from: %1
+echo ###########################################################
 set SCSREV=
 if "%SCSGIT%"=="y" goto :SCSGIT
 if not "%SCSTAG%"=="" goto :GOTTAG
@@ -16,42 +16,30 @@ goto :SCSREV
 call :GETREV "%~1"
 :SCSREV
 if "%SCSREV%"=="" goto :END
-for /f "delims=| tokens=1,2,3" %%a in ("%SCSREV%") do set SVNDAT=%%c
-set SVNDAT=%SVNDAT: =%
-set hour=%SVNDAT:~10,2%
-set min=%SVNDAT:~13,2%
-set sec=%SVNDAT:~16,2%
-set timezone=(UTC%SVNDAT:~18,3%:%SVNDAT:~21,2%)
-set dd=%SVNDAT:~8,2%
-set mm=%SVNDAT:~5,2%
-set yyyy=%SVNDAT:~0,4%
-set mmm=%SVNDAT:~30,3%
+for /f "delims=| tokens=1,2,3" %%a in ("%SCSREV%") do set SCSDAT=%%c
 set SCSREV=%SCSREV:~1,5%
-if "%~2"=="" goto :END
-%SVNLOG:"=% %SVNLPR:"=% %SCSREV% %1 > %2
-goto :END
+set SCSDAT=%SCSDAT: =%
+goto :SCSDAT
 :GETTAG
 set SCSPTH=%1
 set CURMAJ=0
 set CURMIN=0
-for /f %%i in ('%SVNLST:"=% %SCSPTH:"=%') do call :SCSTAG %%i
+for /f %%i in ('%SVNLST:"=% %SCSPTH:"=%') do call :SVNTAG %%i
 exit /b 0
-:SCSTAG
+:SVNTAG
 set NEWTAG=%1
 set NEWTAG=%NEWTAG:~0,-1%
 set TAGDEX=1
 setlocal enabledelayedexpansion
-:TAGLOP
-if "!NEWTAG:~%TAGDEX%,1!"=="." goto :TAGDOT
+:SVNLOP
+if "!NEWTAG:~%TAGDEX%,1!"=="." goto :SVNDOT
 set /a TAGDEX+=1
-goto :TAGLOP
-:TAGDOT
+goto :SVNLOP
+:SVNDOT
 set NEWMAJ=!NEWTAG:~0,%TAGDEX%!
 set /a TAGDEX+=1
 set NEWMIN=!NEWTAG:~%TAGDEX%!
-endlocal & set NEWMAJ=%NEWMAJ% & set NEWMIN=%NEWMIN%
-set NEWMAJ=%NEWMAJ: =%
-set NEWMIN=%NEWMIN: =%
+endlocal & set NEWMAJ=%NEWMAJ%& set NEWMIN=%NEWMIN%
 if %NEWMAJ% LSS %CURMAJ% exit /b 0
 if %NEWMIN% LSS %CURMIN% exit /b 0
 set CURMAJ=%NEWMAJ%
@@ -65,12 +53,11 @@ exit /b 0
 set SCSPTH=%~1
 if "%SCSPTH:~1,1%"==":" (%SCSPTH:~0,2% && cd %SCSPTH:~2%
 ) else cd %SCSPTH%
+if "%SCSTAG%"=="" for /f "tokens=*" %%i in ('git tag') do set SCSTAG=%%i
 for /f "tokens=*" %%i in ('git log -1 --pretty^=format:"%%h"') do set SCSREV=%%i
 for /f "tokens=*" %%i in ('git log -1 --pretty^=format:"%%ai"') do set SCSDAT=%%i
+:SCSDAT
 set yyyy=%SCSDAT:~0,4%
 set mm=%SCSDAT:~5,2%
 set dd=%SCSDAT:~8,2%
-for /f "tokens=*" %%i in ('git tag') do set SCSTAG=%%i
-if "%~2"=="" goto :END
-for /f "tokens=*" %%i in ('git log -1 --pretty^=format:"%%s"') do echo %%i > %2
 :END
